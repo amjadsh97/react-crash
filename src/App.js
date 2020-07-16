1f8efd0546c89145a0636237a7b77b1706bdc606
Amjad Shadid <amjadshadid51@gmail.com>
2020-07-16 09:55:45 +0300

    Hook in react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import HelloWorld from "./Components/HelloWorld";
+import CounterExample from "./Components/CounterExample";
+
+function App() {
+  return (
+      <div>
+        {/*<HelloWorld name="amjad"/>*/}
+        <CounterExample/>
+      </div>
+  );
+}
+
+export default App;
